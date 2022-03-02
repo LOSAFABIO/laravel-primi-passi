833cd536d5ca6d0ac537c45b94011632d0bb8820
LOSAFABIO <fabio.losa95@gmail.com>
2022-03-02 16:41:11 +0100

    ok finito

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: rgb(202, 202, 202);
+                color: #4591b8;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            body {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            h1 {
+                font-size: 84px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
